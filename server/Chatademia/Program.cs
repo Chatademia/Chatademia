14439d47c90dcda7d254cb6bc84f1826628313ab
jk-git-work <jakub.kaminski.work@gmail.com>
2025-11-12 19:21:05 +0100

    Add Docker setup for backend

--- server/Chatademia/Program.cs
+++ server/Chatademia/Program.cs
@@ -7,17 +7,16 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.Urls.Add("http://0.0.0.0:8080");
 
 app.Run();
