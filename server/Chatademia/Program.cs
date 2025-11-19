6de0e7b8b36203104f5c7b4bb2bc5961858bb4b7
Wojciech Kalota <wojciech.kalota1@gmail.com>
2025-11-19 02:55:03 +0100

    Add USOS key

--- server/Chatademia/Program.cs
+++ server/Chatademia/Program.cs
@@ -9,14 +9,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddScalar();
 builder.Services.AddOpenApi();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddTransient<AuthServices>();
+
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
--- server/Chatademia/Services/AuthServices.cs
+++ server/Chatademia/Services/AuthServices.cs
@@ -5,10 +5,16 @@ namespace chatademia.Services
     public class AuthServices
     {
         private AppDbContext _context;
+        private readonly string _USOS_KEY;
+        private readonly string _USOS_SECRET;
 
-        public AuthServices(AppDbContext context)
+        public AuthServices(AppDbContext context, IConfiguration config)
         {
             _context = context;
+            _USOS_KEY = config["USOS_KEY"] ?? 
+                throw new ArgumentNullException("USOS_KEY is missing from configuration");
+            _USOS_SECRET = config["USOS_SECRET"] ?? 
+                throw new ArgumentNullException("USOS_SECRET is missing from configuration");
         }
 
         public void Login(int PIN)
