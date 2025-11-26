cb53093989fa3933e027b92ea06b14a1ed7529a0
Aleksander Radecki <alerksanderradecki@gmail.com>
2025-11-26 20:14:01 +0100

    restructured  - added routes

--- webapp/chatademia/src/pages/Chat.jsx
+++ webapp/chatademia/src/pages/Chat.jsx
@@ -1,21 +1,23 @@
-
+import icon from "../assets/icon.png";
+import arrowDown from "../assets/arrowDown.svg";
+import plus from "../assets/plus.svg"
 
 function Chat() {
 
     return (
         <div className="bg-white flex h-screen">
-            <div className="w-1/4 border p-4 flex-col">
-                <div className="border flex gap-2 pr-4">
-                    <img src="../assets/icon.png" alt="Logo" className="h-8 w-8"/>
-                    <h1 className="text-2xl font-bold text-primary">Chatademia</h1>
+            <div className="w-1/4 border flex flex-col">
+                <div className=" flex gap-2 pr-4  p-5 border-b items-center">
+                    <img src={icon} alt="Logo" className="h-12 w-12"/>
+                    <h1 className="text-4xl font-bold text-primary">Chatademia</h1>
                 </div>
-                <div className="border flex gap-2 pt-2 justify-between items-center">
-                    <div className="flex gap-1 items-center">
+                <div className=" flex gap-2 justify-between items-center p-5">
+                    <div className="flex gap-2 items-center">
                         <h1 className="font-semibold text-black text-xl">Czaty grupowe</h1>
-                        <img src="../assets/arrowDown.svg" alt="arrow down" className="h-4 w-4"/>
+                        <img src={arrowDown} alt="arrow down" className="h-5 w-5"/>
                     </div> 
-                    <div className="rounded-full bg-primary text-white">
-                        <h1>+</h1>
+                    <div className="rounded-full bg-primary text-white overflow-visible h-8 w-8 flex justify-center items-center cursor-pointer">
+                        <img src={plus} alt="plus" className="h-6 w-6"/>
                     </div>
 
                 </div>
@@ -30,3 +32,5 @@ function Chat() {
         </div>
     )
 }
+
+export default Chat;
