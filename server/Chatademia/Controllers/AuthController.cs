16385bfa68f35ef022553bdbefe6a9438d06dbcc
jk-git-work <jakub.kaminski.work@gmail.com>
2025-12-10 16:05:09 +0100

    added back end cookie with sesion guid in it

--- server/Chatademia/Controllers/AuthController.cs
+++ server/Chatademia/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Chatademia.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Web;
 
 namespace chatademia.Controllers
 {
@@ -21,6 +22,16 @@ namespace chatademia.Controllers
         public async Task<IActionResult> Login([FromBody] SessionRequestVM request)
         {
             var session = await _loginServices.Login(request.OauthToken, request.OauthVerifier);
+            Response.Cookies.Append(
+            "AuthSessionId",
+            session.Session.ToString(),
+            new CookieOptions
+            {
+                HttpOnly = true,
+                Secure = false,
+                SameSite = SameSiteMode.Lax,
+                Expires = DateTimeOffset.UtcNow.AddHours(1)
+            });
             return Ok(session);
         }
 
